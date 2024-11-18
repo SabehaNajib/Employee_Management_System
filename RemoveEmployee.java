ee130614e91eddeb52a44330e8ec2cb12dc6b7d0
SabehaNajib <137303856+SabehaNajib@users.noreply.github.com>
2024-11-18 18:00:29 +0530

    Update RemoveEmployee.java

--- RemoveEmployee.java
+++ RemoveEmployee.java
@@ -36,83 +36,83 @@ public class RemoveEmployee extends JFrame implements ActionListener{
 		}
 		
 		JLabel labelname = new JLabel("Name");
-        labelname.setBounds(50, 100, 100, 30);
-        add(labelname);
+	        labelname.setBounds(50, 100, 100, 30);
+	        add(labelname);
+	        
+	        JLabel lblname = new JLabel();
+	        lblname.setBounds(200, 100, 100, 30);
+	        add(lblname);
+	        
+	        JLabel labelnumber = new JLabel("Contact number");
+	        labelnumber.setBounds(50, 150, 100, 30);
+	        add(labelnumber);
+	        
+	        JLabel lblnumber = new JLabel();
+	        lblnumber.setBounds(200, 150, 100, 30);
+	        add(lblnumber);
+	        
+	        JLabel labelemail = new JLabel("Email");
+	        labelemail.setBounds(50, 200, 100, 30);
+	        add(labelemail);
+	        
+	        JLabel lblemail = new JLabel();
+	        lblemail.setBounds(200, 200, 100, 30);
+	        add(lblemail);
         
-        JLabel lblname = new JLabel();
-        lblname.setBounds(200, 100, 100, 30);
-        add(lblname);
-        
-        JLabel labelnumber = new JLabel("Contact number");
-        labelnumber.setBounds(50, 150, 100, 30);
-        add(labelnumber);
-        
-        JLabel lblnumber = new JLabel();
-        lblnumber.setBounds(200, 150, 100, 30);
-        add(lblnumber);
-        
-        JLabel labelemail = new JLabel("Email");
-        labelemail.setBounds(50, 200, 100, 30);
-        add(labelemail);
-        
-        JLabel lblemail = new JLabel();
-        lblemail.setBounds(200, 200, 100, 30);
-        add(lblemail);
-        
-        try {
+       		 try {
 			DatabaseConnection con = new DatabaseConnection();
 			String query = "SELECT * FROM employees WHERE empId = '"+cEmpId.getSelectedItem()+"'";
 			ResultSet rs = con.stm.executeQuery(query);
 			
 			while(rs.next()) {
 				lblname.setText(rs.getString("name"));
-                lblnumber.setText(rs.getString("contact_number"));
-                lblemail.setText(rs.getString("email"));
+		                lblnumber.setText(rs.getString("contact_number"));
+		                lblemail.setText(rs.getString("email"));
 			}
 		}
 		catch(Exception e) {
 			e.printStackTrace();
 		}
         
-        cEmpId.addItemListener(new ItemListener() {
-        	public void itemStateChanged(ItemEvent ie) {
-        		try {
-        			DatabaseConnection con = new DatabaseConnection();
-        			String query = "SELECT * FROM employees WHERE empId = '"+cEmpId.getSelectedItem()+"'";
-        			ResultSet rs = con.stm.executeQuery(query);
-        			
-        			while(rs.next()) {
-        				lblname.setText(rs.getString("name"));
-                        lblnumber.setText(rs.getString("contact_number"));
-                        lblemail.setText(rs.getString("email"));
-        			}
-        		}
-        		catch(Exception e) {
-        			e.printStackTrace();
-        		}
-        	}
-        });
+	        cEmpId.addItemListener(new ItemListener() {
+	        	public void itemStateChanged(ItemEvent ie) {
+	        		try {
+	        			DatabaseConnection con = new DatabaseConnection();
+	        			String query = "SELECT * FROM employees WHERE empId = '"+cEmpId.getSelectedItem()+"'";
+	        			ResultSet rs = con.stm.executeQuery(query);
+	        			
+	        			while(rs.next()) {
+	        				lblname.setText(rs.getString("name"));
+			                        lblnumber.setText(rs.getString("contact_number"));
+			                        lblemail.setText(rs.getString("email"));
+	        			}
+	        		}
+	        		catch(Exception e) {
+	        			e.printStackTrace();
+	        		}
+	        	}
+	        });
         
-        delete = new JButton("Delete");
-        delete.setBounds(80, 300, 100,30);
-        delete.setBackground(Color.BLACK);
-        delete.setForeground(Color.WHITE);
-        delete.addActionListener(this);
-        add(delete);
-        
-        back = new JButton("Back");
-        back.setBounds(220, 300, 100,30);
-        back.setBackground(Color.BLACK);
-        back.setForeground(Color.WHITE);
-        back.addActionListener(this);
-        add(back);
-        
+	        delete = new JButton("Delete");
+	        delete.setBounds(80, 300, 100,30);
+	        delete.setBackground(Color.BLACK);
+	        delete.setForeground(Color.WHITE);
+	        delete.addActionListener(this);
+	        add(delete);
+	        
+	        back = new JButton("Back");
+	        back.setBounds(220, 300, 100,30);
+	        back.setBackground(Color.BLACK);
+	        back.setForeground(Color.WHITE);
+	        back.addActionListener(this);
+	        add(back);
+	        
 		ImageIcon i1 = new ImageIcon("Images/EMS3.png");
-        Image i2 = i1.getImage().getScaledInstance(600, 400, Image.SCALE_DEFAULT);
-        ImageIcon i3 = new ImageIcon(i2);
-        JLabel image = new JLabel(i3);
-        image.setBounds(350, 0, 600, 400);
-        add(image);
+	        Image i2 = i1.getImage().getScaledInstance(600, 400, Image.SCALE_DEFAULT);
+	        ImageIcon i3 = new ImageIcon(i2);
+	        JLabel image = new JLabel(i3);
+	        image.setBounds(350, 0, 600, 400);
+	        add(image);
         
 		
 		setSize(1000, 500);
